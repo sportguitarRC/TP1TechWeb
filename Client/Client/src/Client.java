931e27f8c219ecaa13ae3c4760001075a9c84c72
Rafaël Côté <raf87crosby@hotmail.com>
2017-01-24 15:44:18 -0500

    petit avancement!

--- Client/Client/src/Client.java
+++ Client/Client/src/Client.java
@@ -1,9 +1,33 @@
+import java.net.*;
+
 import javax.swing.*;
 public class Client {
 
+	public static void StartClient() throws Exception
+	{
+		int port = 80;
+		String adresse = JOptionPane.showInputDialog("Entrer l'adresse web:");
+		try
+		{
+			InetAddress aa = InetAddress.getByName(adresse);
+			JOptionPane.showMessageDialog(null, aa.getHostAddress());
+		}
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(null, "Ecris un site web valide criss de retard");
+		}
+		Socket socket = new Socket(adresse, port);
+	}
+	
 	public static void main(String [] args)
 	{
-		JOptionPane.showMessageDialog(null, "Nich est cave");
-		
+		try
+		{
+			StartClient();
+		}
+		catch (Exception e)
+		{
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
 	}
 }
--- Serveur/src/Server.java
+++ Serveur/src/Server.java
@@ -1,10 +1,37 @@
+import java.net.*;
+
 import javax.swing.JOptionPane;
+
+import java.io.*;
 
 public class Server {
 	
+	public static void StartServeur() throws Exception
+	{
+		int noPort = 80;
+		
+		ServerSocket serverSocket = new ServerSocket(noPort);
+		while(true) //infinite loops
+		{
+			Socket connexionSocket = serverSocket.accept();
+			
+			connexionSocket.close();
+			break;
+		}
+		
+		serverSocket.close();
+	}
+	
 	public static void main(String [] args)
 	{
-		JOptionPane.showMessageDialog(null, "Nich est laite");
+		try
+		{
+			StartServeur();
+		}
+		catch (Exception e)
+		{
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
 		
 	}
 }
