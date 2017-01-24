271262370a007c5207d243ae581688f13cafe173
Rafaël Côté <raf87crosby@hotmail.com>
2017-01-24 15:01:47 -0500

    Creation de projet
    Projet qui marche ( javax.swing )

---
+++ Client/Client/src/Client.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Client {
+
+	public static void main(String [] args)
+	{
+		JOptionPane.showMessageDialog(null, "Nich est cave");
+		
+	}
+}
---
+++ Serveur/src/Server.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Server {
+	
+	public static void main(String [] args)
+	{
+		JOptionPane.showMessageDialog(null, "Nich est laite");
+		
+	}
+}
