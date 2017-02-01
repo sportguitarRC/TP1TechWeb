1cb73191d163e709d77613e7c6d39931fcb48c28
Emile Fortin <sylfschiffer@gmail.com>
2017-02-01 17:50:29 -0500

    Connexion possible

--- Client/Client/src/Client.java
+++ Client/Client/src/Client.java
@@ -7,46 +7,43 @@ public class Client {
 	{
 		ObjectOutputStream out;
 		ObjectInputStream in;
+		PrintWriter print;
 		
 		int port = 80;
-		String message;
 		String adresse = JOptionPane.showInputDialog("Entrer l'adresse web:");
 		try
 		{
 			InetAddress inetAdd = InetAddress.getByName(adresse);
-			JOptionPane.showMessageDialog(null, inetAdd.getHostAddress());
-			
 			Socket socket = new Socket(inetAdd, port);
 			
 			out = new ObjectOutputStream(socket.getOutputStream());
 			out.flush();
+			print = new PrintWriter(out);
 			
 			try
 			{
-				in = new ObjectInputStream(socket.getInputStream());
-				message = (String)in.readObject();
+				print.println("Get / HTTP/1.1");
+				print.println("Host: " + adresse);
+				print.println("Accept: text/plain , image/*");
+				print.println("");
+				print.flush();
 				
-				message = "GET HTTP/1.0\n" +
-						"Accept: text/plain , image/*\n" +
-						"If-Modified-Since: Wed, 10 Sep 2015 14:20:21 GMT\n" +
-						"Referer: http://www.uqac.ca/index.html\n" +
-						"User-Agent: Chrome/55.0.2883.87\n\n";
-				out.writeObject(message);
-				out.flush();
-				
-				
-				JOptionPane.showMessageDialog(null, message);
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String st;
+				System.out.println(br.readLine());
+				while((st = br.readLine()) != null)
+					System.out.println(st);
+				br.close();
 			}
 			catch (Exception e)
 			{
 				JOptionPane.showMessageDialog(null, e.getMessage());
 			}
-			
 			socket.close();
 		}
 		catch(Exception e)
 		{
-			JOptionPane.showMessageDialog(null, "Ecris un site web valide.");
+			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
 	}
 	
