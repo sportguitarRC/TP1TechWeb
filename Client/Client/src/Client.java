6a795398df3aa6db3a6a189ff76ef80db62d1266
Emile Fortin <sylfschiffer@gmail.com>
2017-02-01 22:07:03 -0500

    Terminé + commenté le code de client.java

--- Client/Client/src/Client.java
+++ Client/Client/src/Client.java
@@ -3,18 +3,18 @@ import java.io.*;
 import javax.swing.*;
 public class Client {
 
-	public static void StartClient() throws Exception
+	public static void StartClient() throws Exception // Méthode principale utilisée par le programme pour retrouver une page web
 	{
-		ObjectOutputStream out;
-		PrintWriter print;
+		ObjectOutputStream out; // stream de sortie pour envoyer une requete
+		PrintWriter print; // point de contact avec le serveur distant
 		
-		int port = 80;
-		String adresse = JOptionPane.showInputDialog("Entrer l'adresse du site web ( Ex : www.uqac.ca )");
-		String page = JOptionPane.showInputDialog("Entrez la page recherchee ( Ex : etudiants/");
+		int port = 80; // port utilisél, 80 car requete html
+		String adresse = JOptionPane.showInputDialog("Entrer l'adresse du site web ( Ex : www.uqac.ca )"); // Domaine recherché par le dns
+		String page = JOptionPane.showInputDialog("Entrez la page recherchee ( Ex : etudiants/"); // Emplacement de la page voulue sur le serveur
 		try
 		{
-			InetAddress inetAdd = InetAddress.getByName(adresse);
-			Socket socket = new Socket(inetAdd, port);
+			InetAddress inetAdd = InetAddress.getByName(adresse); // Recherche de l'adresse IP par le serveur dns
+			Socket socket = new Socket(inetAdd, port); // Socket de connexion client
 			
 			out = new ObjectOutputStream(socket.getOutputStream());
 			out.flush();
@@ -22,19 +22,29 @@ public class Client {
 			
 			try
 			{
+				// Début de la requête html basique
 				print.println("Get /" + page + " HTTP/1.1");
-				print.println("Get / HTTP/1.1");
 				print.println("Host: " + adresse);
 				print.println("Accept: text/plain , image/*");
 				print.println("");
 				print.flush();
 				
+				// Création du lecteur pour recevoir la page html
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				String st;
-				System.out.println(br.readLine());
-				while((st = br.readLine()) != null)
-					System.out.println(st);
-				br.close();
+				boolean html  = false; // variable utilisée pour savoir si on a franchi le seuil d'affichage du html
+				PrintWriter writer = new PrintWriter("page.html", "UTF-8"); // écriture de la page html recue
+
+				while((st = br.readLine()) != null) // Lecture tant qu'il reste des lignes dans la réception
+				{
+					if(html)
+						writer.println(st); // Écriture dans un fichier
+					if(st.isEmpty()) // Détecte qu'il y a une vide nulle et donc qu'on peut commencer a enregistrer
+						html = true;
+					//System.out.println(st); // ligne pouvant être rajoutée pour le debug
+				}
+				br.close(); // fermeture du reader
+				writer.close(); // fermeture de l'ecriture sur le fichier .html
 			}
 			catch (Exception e)
 			{
