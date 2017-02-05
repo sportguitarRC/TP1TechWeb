42e32639e54d50ad5dbb6e67ea349c294ec8c8a3
Emile Fortin <sylfschiffer@gmail.com>
2017-02-04 22:33:20 -0500

    Possibilité d'envoyer une page par 127.0.0.1

--- Serveur/src/Server.java
+++ Serveur/src/Server.java
@@ -18,39 +18,42 @@ public class Server {
 				PrintWriter print  = new PrintWriter(new OutputStreamWriter(connexionSocket.getOutputStream()));
 				BufferedReader br = new BufferedReader(new InputStreamReader(connexionSocket.getInputStream()));
 				
-				String st;
-				while((st = br.readLine()) != null) // Lecture tant qu'il reste des lignes dans la réception
+				String st = br.readLine();
+				while(st.hashCode() != 0) // Lecture tant qu'il reste des lignes dans la réception
 				{
 					System.out.println(st); // ligne pouvant être rajoutée pour le debug
+					st = br.readLine();
 				}
 				
-				
-
-				BufferedReader reader = new BufferedReader(new FileReader("Files\\index.html"));
-				
-				print.println("HTTP/1.0 200 OK");
-				print.println("Date: Wed, 10 Sep 2015 14:20:21 GMT");
-				print.println("Server: NCSA/1.5.2");
-				print.println("Mime-Vesion: 1.0");
-				print.println("Content-Type: text/html");
-				print.println("Last-Modified: Wed, 10 Sep 2015 14:20:23 GMT");
-				print.println("Content-Length: 378");
-				print.println("");
+				System.out.println("Debug");
 				try 
 				{
-					    String line = br.readLine();
-		
-					    while (line != null) 
-					    {
-					        print.print(line);
-					        line = br.readLine();
-				    	}
+					BufferedReader reader = new BufferedReader(new FileReader("Files\\index.html"));
+					
+					print.println("HTTP/1.0 200 OK");
+					print.println("Date: Wed, 10 Sep 2015 14:20:21 GMT");
+					print.println("Server: NCSA/1.5.2");
+					print.println("Mime-Vesion: 1.0");
+					print.println("Content-Type: text/html");
+					print.println("Last-Modified: Wed, 10 Sep 2015 14:20:23 GMT");
+					print.println("Content-Length: 205");
+					print.println("");
+					
+				    String line = reader.readLine();
+			
+				    while (line != null) 
+				    {
+				        print.print(line);
+				        System.out.println(line); // ligne pouvant être rajoutée pour le debug
+				        line = reader.readLine();
+			    	}
+					reader.close();
 				}
 				catch(Exception e)
 				{
 				    JOptionPane.showMessageDialog(null, e.getMessage());
 			    }
-				reader.close();
+				print.flush();
 				connexionSocket.close();
 			}
 		}
