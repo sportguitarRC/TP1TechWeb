253fba6097a184baad2d05b78c8d474aa3d6a692
Emile Fortin <sylfschiffer@gmail.com>
2017-02-05 14:23:54 -0500

    Fin du programme, ajout de debug, trace et manuel d'utilisation.

--- Client/Client/src/Client.java
+++ Client/Client/src/Client.java
@@ -3,8 +3,11 @@ import java.io.*;
 import javax.swing.*;
 public class Client {
 
+	static boolean debug_console = false;
+	
 	public static void StartClient() throws Exception // Méthode principale utilisée par le programme pour retrouver une page web
 	{
+		if(debug_console) System.out.println("Entrée dans StartClient() .");
 		ObjectOutputStream out; // stream de sortie pour envoyer une requete
 		PrintWriter print; // point de contact avec le serveur distant
 		
@@ -13,7 +16,9 @@ public class Client {
 		String page = JOptionPane.showInputDialog("Entrez la page recherchee ( Ex : etudiants/"); // Emplacement de la page voulue sur le serveur
 		try
 		{
+			if(debug_console) System.out.println("Recherche de l'adresse par serveur DNS.");
 			InetAddress inetAdd = InetAddress.getByName(adresse); // Recherche de l'adresse IP par le serveur dns
+			if(debug_console) System.out.println("IP : " + inetAdd.getHostAddress());
 			Socket socket = new Socket(inetAdd, port); // Socket de connexion client
 			
 			out = new ObjectOutputStream(socket.getOutputStream());
@@ -22,14 +27,16 @@ public class Client {
 			
 			try
 			{
+				if(debug_console) System.out.println("Requête HTML :");
 				// Début de la requête html basique
-				print.println("Get /" + page + " HTTP/1.1");
-				print.println("Host: " + adresse);
-				print.println("Accept: text/plain , image/*");
-				print.println("");
+				print.println("Get /" + page + " HTTP/1.1");  if(debug_console) System.out.println("Get /" + page + " HTTP/1.1");
+				print.println("Host: " + adresse); if(debug_console) System.out.println("Host: " + adresse);
+				print.println("Accept: text/plain , image/*"); if(debug_console) System.out.println("Accept: text/plain , image/*");
+				print.println(""); if(debug_console) System.out.println("");
 				print.flush();
 				
 				// Création du lecteur pour recevoir la page html
+				if(debug_console) System.out.println("Création de page.html");
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				String st;
 				boolean html  = false; // variable utilisée pour savoir si on a franchi le seuil d'affichage du html
@@ -38,13 +45,14 @@ public class Client {
 				while((st = br.readLine()) != null) // Lecture tant qu'il reste des lignes dans la réception
 				{
 					if(html)
-						writer.println(st); // Écriture dans un fichier
+						writer.println(st);  // Écriture dans un fichier
 					if(st.isEmpty()) // Détecte qu'il y a une vide nulle et donc qu'on peut commencer a enregistrer
 						html = true;
-					//System.out.println(st); // ligne pouvant être rajoutée pour le debug
+					if(debug_console) System.out.println(st);
 				}
 				br.close(); // fermeture du reader
 				writer.close(); // fermeture de l'ecriture sur le fichier .html
+				if(debug_console) System.out.println("Fermeture du socket et du programme");
 			}
 			catch (Exception e)
 			{
@@ -60,6 +68,7 @@ public class Client {
 	
 	public static void main(String [] args)
 	{
+		if(debug_console) System.out.println("Démarrage du programme.");
 		try
 		{
 			StartClient();
@@ -68,5 +77,6 @@ public class Client {
 		{
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
+		if(debug_console) System.out.println("Fin du main");
 	}
 }
--- Serveur/src/Server.java
+++ Serveur/src/Server.java
@@ -1,60 +1,79 @@
 import java.net.*;
-
 import javax.swing.JOptionPane;
-
 import java.io.*;
+import java.util.*;
 
 public class Server {
+
+	static boolean debug_console = false;
 	
 	public static void StartServeur() throws Exception
 	{
+		if(debug_console) System.out.println("Entrée dans StartServeur()");
 		int noPort = 80;
 		try
 		{
-			ServerSocket serverSocket = new ServerSocket(noPort);
+			ServerSocket serverSocket = new ServerSocket(noPort); // start socket that will accept connexions
+			if(debug_console) System.out.println("Écoute sur port" + noPort);
 			while(true) //infinite loops
 			{
-				Socket connexionSocket = serverSocket.accept();
-				PrintWriter print  = new PrintWriter(new OutputStreamWriter(connexionSocket.getOutputStream()));
-				BufferedReader br = new BufferedReader(new InputStreamReader(connexionSocket.getInputStream()));
+				Socket connexionSocket = serverSocket.accept(); // socket to communicate with one client
+				if(debug_console) System.out.println("Connexion établie avec un client");
+				PrintWriter print  = new PrintWriter(new OutputStreamWriter(connexionSocket.getOutputStream())); // send data to client
+				BufferedReader br = new BufferedReader(new InputStreamReader(connexionSocket.getInputStream())); // receive data from client
 				
-				String st = br.readLine();
-				while(st.hashCode() != 0) // Lecture tant qu'il reste des lignes dans la réception
+				String st = br.readLine();  // read first line, with file name
+				st = st.substring(5, st.length() - 9);
+				String filename;
+				if(st.hashCode() == 0 || st == "/" )
+					filename = "index.html";
+				else filename = st;
+				while(st.hashCode() != 0) // listen as long as there are lines transiting
 				{
-					System.out.println(st); // ligne pouvant être rajoutée pour le debug
+					if(debug_console) System.out.println(st);
 					st = br.readLine();
 				}
 				
-				System.out.println("Debug");
 				try 
 				{
-					BufferedReader reader = new BufferedReader(new FileReader("Files\\index.html"));
+					BufferedReader reader;
+					if (new File("Files\\" + filename).exists()) // check if file exists
+						{  reader = new BufferedReader(new FileReader("Files\\" + filename));} // Open the file the client asked for
+						else { reader = new BufferedReader(new FileReader("Files\\404.html"));} // Return a 404 error
+
+					if(debug_console) System.out.println("Envoi de " + filename);
 					
-					print.println("HTTP/1.0 200 OK");
-					print.println("Date: Wed, 10 Sep 2015 14:20:21 GMT");
-					print.println("Server: NCSA/1.5.2");
-					print.println("Mime-Vesion: 1.0");
-					print.println("Content-Type: text/html");
-					print.println("Last-Modified: Wed, 10 Sep 2015 14:20:23 GMT");
-					print.println("Content-Length: 205");
+					// HTML PROTOCOL DATA 1.0 SENT TO CLIENT
+					print.println("HTTP/1.0 200 OK"); if(debug_console) System.out.println("HTTP/1.0 200 OK");
+					Calendar cal = Calendar.getInstance();
+					String months[] = {"Jan","Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+					String days [] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+					print.println("Date: " + days[cal.get(Calendar.DAY_OF_WEEK) - 1] + ", " + cal.get(Calendar.DAY_OF_MONTH) + " " + months[cal.get(Calendar.MONTH)] + " " + cal.get(Calendar.YEAR) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + " EST"); if(debug_console) System.out.println("Date: " + days[cal.get(Calendar.DAY_OF_WEEK) - 1] + ", " + cal.get(Calendar.DAY_OF_MONTH) + " " + months[cal.get(Calendar.MONTH)] + " " + cal.get(Calendar.YEAR) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + " EST");
+					print.println("Server: NCSA/1.5.2"); if(debug_console) System.out.println("Server: NCSA/1.5.2");
+					print.println("Mime-Vesion: 1.0"); if(debug_console) System.out.println("Mime-Vesion: 1.0");
+					print.println("Content-Type: text/html"); if(debug_console) System.out.println("Content-Type: text/html");
+					print.println("Last-Modified: Sun, 5 Feb 2017 11:00:00 EST"); if(debug_console) System.out.println("Last-Modified: Sun, 5 Feb 2017 11:00:00 EST");
 					print.println("");
 					
+					// Send HTML file line by line
 				    String line = reader.readLine();
-			
 				    while (line != null) 
 				    {
 				        print.print(line);
-				        System.out.println(line); // ligne pouvant être rajoutée pour le debug
+				        if(debug_console) System.out.println(line);
 				        line = reader.readLine();
 			    	}
-					reader.close();
+				    
+					reader.close(); // close file reader
+					if(debug_console) System.out.println("Fin de l'envoi");
 				}
 				catch(Exception e)
 				{
 				    JOptionPane.showMessageDialog(null, e.getMessage());
 			    }
 				print.flush();
-				connexionSocket.close();
+				connexionSocket.close(); // close the specific client socket
+				if(debug_console) System.out.println("Connexion avec le client terminée");
 			}
 		}
 		catch(Exception e)
@@ -65,14 +84,15 @@ public class Server {
 	
 	public static void main(String [] args)
 	{
+		if(debug_console) System.out.println("Entrée dans le main.");
 		try
 		{
-			StartServeur();
+			StartServeur(); // Start the server loop
 		}
 		catch (Exception e)
 		{
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
-		
+		if(debug_console) System.out.println("Fin du main.");
 	}
 }
